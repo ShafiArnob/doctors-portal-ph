5d6cefd46b9e942fab15e02068b5d96310174813
ShafiArnob <shafiarnob@gmail.com>
2022-05-15 22:02:15 +0600

    bugfix--footer

--- doctors-appointment-client/src/Pages/Home/Home.js
+++ doctors-appointment-client/src/Pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Banner from './Banner';
+import Footer from './Footer';
 import Info from './Info';
 import MakeAppointment from './MakeAppointment';
 import Services from './Services';
@@ -11,6 +12,7 @@ const Home = () => {
       <Info></Info>
       <Services></Services>
       <MakeAppointment></MakeAppointment>
+      <Footer></Footer>
     </div>
   );
 };
