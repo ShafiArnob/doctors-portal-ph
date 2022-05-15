d0f27986b486fb2a42810b9dfdf747ab6fc927b7
ShafiArnob <shafiarnob@gmail.com>
2022-05-15 16:13:26 +0600

    Hello World

---
+++ doctors-appointment-client/src/App.js
@@ -0,0 +1,20 @@
+import Navbar from './Pages/Shared/Navbar'
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
+
+function App() {
+  
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
