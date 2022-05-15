35958e63795dd08de60444c643015eb66a4532a1
ShafiArnob <shafiarnob@gmail.com>
2022-05-15 16:34:43 +0600

    navbar

--- doctors-appointment-client/src/App.js
+++ doctors-appointment-client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
+import Login from './Pages/Login/Login';
 
 function App() {
   
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/about' element={<About></About>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
       </Routes>
     </div>
   );
